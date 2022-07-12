a61d8db2ff16cf821a0558cf3cbe671eb59286a4
Gianluca Mollo Rivas <gianmollo03@gmail.com>
2022-07-12 10:26:46 -0300

    Arreglo el mail

--- scripts/script.js
+++ scripts/script.js
@@ -16,5 +16,4 @@ formulario.addEventListener("submit",(eventoFormulario) =>{
     console.log(` ${inputNombre.value} - ${inputApellido.value} - ${inputEmail.value} - ${inputCajaTexto.value}`)
     formulario.style = "visibility:hidden"
     respuestaFormulario.style = "visibility:visible"
-
 })  
